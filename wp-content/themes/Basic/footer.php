31537b96e9a86bc18826b374751e205e03a92e85
Alexandr Ivanov <aivanov@pruffi.ru>
2013-03-19 15:10:21 +0400

    Reinit

--- wp-content/themes/Basic/footer.php
+++
@@ -1,6 +0,0 @@
-<div style="clear: both;"></div>
-</div>
-<div id="footer"></div>
-
-<?php get_template_part('includes/scripts'); ?>
-<?php wp_footer(); ?>
--- wp-content/themes/Basic/sidebar.php
+++
@@ -1,9 +0,0 @@
-<div id="sidebar">
-
-    <div id="header"><a href="<?php bloginfo('url'); ?>" title="<?php bloginfo('name'); ?>"> <?php $logo = (get_option('basic_logo') <> '') ? get_option('basic_logo') : get_bloginfo('template_directory').'/images/logo-'.get_option('basic_color_scheme').'.gif';?>
-	<img src="<?php echo $logo; ?>" alt="<?php echo esc_attr(get_bloginfo('name')); ?>" class="logo"/></a> </div>
-	  
-    <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar("Sidebar") ) : ?>
-    <?php endif; ?>
-</div>
-</div>
